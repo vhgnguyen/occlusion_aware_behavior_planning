ccd8a6804b982cdacbd0c58668cc614c6d67b865
vhgnguyen <43420326+vhgnguyen@users.noreply.github.com>
2020-02-28 18:42:24 +0100

    update

--- src/core.py
+++ src/core.py
@@ -3,6 +3,23 @@ import numpy as np
 
 class Core(object):
 
-    def __init__(self):
-        self.egoCar = None
-        self.env = None
+    def __init__(self, env):
+        self._egoCar = None
+        self._env = env
+
+    def addEgoVehicle(self, egoCar):
+        self._egoCar = egoCar
+    
+    def addOtherVehicle(self, otherCar):
+        self._env.addVehicle(otherCar)
+    
+    def addPedestrian(self, pedestrian):
+        self._env.addPedestrian(pedestrian)
+
+    def setScenario(self, nr):
+        self._env.setScenario(nr)
+
+    def update(self, timestamp_s):
+        
+        
+
---
+++ src/types/pedestrian.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+class Pedestrian(object):
+
+    def __init__(self, idx):
+        self._idx = idx
+
+    
